ab4847c3a0807af4a62acdae491f3359c62f8298
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2020-07-04 23:48:19 +0500

    Update send.php

--- send.php
+++ send.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['submit'])){
-$to = "feedbackborya@yandex.ru";
+$to = "anrejfrolov680@gmail.com";
 $from = $_POST['email'];
 $first_name = $_POST['first_name'];
 $subject = "Форма отправки сообщений с сайта";
@@ -8,6 +8,6 @@ $message = $first_name . " оставил сообщение:" . "\n\n" . $_POST
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
 echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-echo "<br /><br /><a href='https://ananiewborya.000webhostapp.com'>Вернуться на сайт.</a>";
+echo "<br /><br /><a href='https://boryagames.github.io/FireCats/'>Вернуться на сайт.</a>";
 }
 ?>
