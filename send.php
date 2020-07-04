5c991479f707a8b2680ab1fdaaec4dd9bd6f7537
BoryaGames <67229357+BoryaGames@users.noreply.github.com>
2020-07-04 23:45:48 +0500

    Add files via upload

---
+++ send.php
@@ -0,0 +1,13 @@
+<?php
+if(isset($_POST['submit'])){
+$to = "feedbackborya@yandex.ru";
+$from = $_POST['email'];
+$first_name = $_POST['first_name'];
+$subject = "Форма отправки сообщений с сайта";
+$message = $first_name . " оставил сообщение:" . "\n\n" . $_POST['message'];
+$headers = "From:" . $from;
+mail($to,$subject,$message,$headers);
+echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
+echo "<br /><br /><a href='https://ananiewborya.000webhostapp.com'>Вернуться на сайт.</a>";
+}
+?>
